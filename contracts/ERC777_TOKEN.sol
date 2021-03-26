23ee76195c12d597ac52aedd5835462f6b45abff
Greg Kapka <greg@oraclize.it>
2021-03-26 16:44:18 +0000

    feat(contracts): <- register the ERC777 one of those as an EC777 recipient

--- contracts/ERC777_TOKEN.sol
+++ contracts/ERC777_TOKEN.sol
@@ -1,9 +1,16 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
 
 contract ERC777_TOKEN is ERC777 {
+
     constructor() ERC777("ERC777", "ERC", new address[](0)) public {
         _mint(msg.sender, 1000000, "", "");
+        IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24).setInterfaceImplementer(
+            address(this),
+            keccak256("ERC777TokensRecipient"),
+            address(this)
+        );
     }
 }
