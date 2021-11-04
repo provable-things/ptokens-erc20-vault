3f3c41774a450f8997531857b3d446410497efb5
Greg Kapka <greg@oraclize.it>
2021-11-04 13:39:38 +0000

    feat(vault-contract): <- make that upgradeable

--- contracts/Erc20Vault.sol
+++ contracts/Erc20Vault.sol
@@ -1,25 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//import "@openzeppelin/contracts-upgradeable/token/ERC777/IERC777Upgradeable.sol";
-import "@openzeppelin/contracts/token/ERC777/IERC777.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import "@openzeppelin/contracts/utils/introspection/IERC1820Registry.sol";
-import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
 import "./IWETH.sol";
 import "./Withdrawable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC777/IERC777Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableSetUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC777/IERC777RecipientUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/introspection/IERC1820RegistryUpgradeable.sol";
 
-contract Erc20Vault is Withdrawable, IERC777Recipient {
-    using SafeERC20 for IERC20;
-    using EnumerableSet for EnumerableSet.AddressSet;
+contract Erc20Vault is
+    Initializable,
+    Withdrawable,
+    IERC777RecipientUpgradeable
+{
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+    using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
 
-    IERC1820Registry constant private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
+    IERC1820RegistryUpgradeable constant private _erc1820 = IERC1820RegistryUpgradeable(
+        0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24
+    );
     bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
     bytes32 constant private Erc777Token_INTERFACE_HASH = keccak256("ERC777Token");
 
-    EnumerableSet.AddressSet private supportedTokens;
+    EnumerableSetUpgradeable.AddressSet private supportedTokens;
     address public PNETWORK;
     IWETH public weth;
 
@@ -31,10 +37,12 @@ contract Erc20Vault is Withdrawable, IERC777Recipient {
         bytes _userData
     );
 
-    constructor(
+    function initialize(
         address _weth,
         address [] memory _tokensToSupport
-    ) {
+    )
+        public initializer
+    {
         PNETWORK = msg.sender;
         for (uint256 i = 0; i < _tokensToSupport.length; i++) {
             supportedTokens.add(_tokensToSupport[i]);
@@ -61,7 +69,7 @@ contract Erc20Vault is Withdrawable, IERC777Recipient {
         PNETWORK = _pnetwork;
     }
 
-    function IS_TOKEN_SUPPORTED(address _token) external view returns(bool) {
+    function isTokenSupported(address _token) external view returns(bool) {
         return supportedTokens.contains(_token);
     }
 
@@ -123,7 +131,7 @@ contract Erc20Vault is Withdrawable, IERC777Recipient {
     {
         require(supportedTokens.contains(_tokenAddress), "Token at supplied address is NOT supported!");
         require(_tokenAmount > 0, "Token amount must be greater than zero!");
-        IERC20(_tokenAddress).safeTransferFrom(msg.sender, address(this), _tokenAmount);
+        IERC20Upgradeable(_tokenAddress).safeTransferFrom(msg.sender, address(this), _tokenAmount);
         emit PegIn(_tokenAddress, msg.sender, _tokenAmount, _destinationAddress, _userData);
         return true;
     }
@@ -202,7 +210,7 @@ contract Erc20Vault is Withdrawable, IERC777Recipient {
         if (_tokenAddress == address(weth)) {
             pegOutWeth(_tokenRecipient, _tokenAmount, "");
         } else {
-            IERC20(_tokenAddress).safeTransfer(_tokenRecipient, _tokenAmount);
+            IERC20Upgradeable(_tokenAddress).safeTransfer(_tokenRecipient, _tokenAmount);
         }
         return true;
     }
@@ -224,7 +232,7 @@ contract Erc20Vault is Withdrawable, IERC777Recipient {
             if (erc777Address == address(0)) {
                 return pegOut(_tokenRecipient, _tokenAddress, _tokenAmount);
             } else {
-                IERC777(erc777Address).send(_tokenRecipient, _tokenAmount, _userData);
+                IERC777Upgradeable(erc777Address).send(_tokenRecipient, _tokenAmount, _userData);
                 return true;
             }
         }
--- contracts/Withdrawable.sol
+++ contracts/Withdrawable.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.24;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "./AbstractOwnable.sol";
 
 abstract contract Withdrawable is AbstractOwnable {
-  using SafeERC20 for IERC20;
+  using SafeERC20Upgradeable for IERC20Upgradeable;
   address constant ETHER = address(0);
 
   event LogWithdrawToken(
@@ -29,7 +29,7 @@ abstract contract Withdrawable is AbstractOwnable {
     if (_tokenAddress == ETHER) {
       payable(msg.sender).transfer(_amount);
     } else {
-      IERC20(_tokenAddress).safeTransfer(msg.sender, _amount);
+      IERC20Upgradeable(_tokenAddress).safeTransfer(msg.sender, _amount);
     }
     emit LogWithdrawToken(msg.sender, _tokenAddress, _amount);
   }
@@ -38,6 +38,6 @@ abstract contract Withdrawable is AbstractOwnable {
   function adminWithdrawAllowed(address asset) internal virtual view returns(uint allowedAmount) {
     allowedAmount = asset == ETHER
       ? address(this).balance
-      : IERC20(asset).balanceOf(address(this));
+      : IERC20Upgradeable(asset).balanceOf(address(this));
   }
 }
