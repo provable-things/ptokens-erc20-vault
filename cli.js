262c50247e4b0100102c18049166b7a88a10c483
Greg Kapka <greg@oraclize.it>
2021-11-05 15:47:13 +0000

    feat(CLI): <- adds fxn to get `weth` address from contract

--- cli.js
+++ cli.js
@@ -7,8 +7,10 @@ const { setPNetwork } = require('./lib/set-pnetwork')
 const { getPNetwork } = require('./lib/get-pnetwork')
 const { deployVault } = require('./lib/deploy-vault')
 const { verifyVault } = require('./lib/verify-vault')
+const { getWEthAddress } = require('./lib/get-weth-address')
 const { flattenContract } = require('./lib/flatten-contract')
 const { showSuggestedFees } = require('./lib/show-suggested-fees')
+const { getSupportedTokens } = require('./lib/get-supported-tokens')
 const { getEncodedInitArgs } = require('./lib/get-encoded-init-args')
 const { showExistingContractAddresses } = require('./lib/show-existing-contract-addresses')
 
@@ -22,10 +24,12 @@ const VERIFY_VAULT_CMD = 'verifyVault'
 const SET_PNETWORK_CMD = 'setPNetwork'
 const GET_PNETWORK_CMD = 'getPNetwork'
 const WETH_ADDRESS_ARG = '<wEthAddress>'
+const GET_WETH_ADDRESS = 'getWEthAddress'
 const FLATTEN_CONTRACT_CMD = 'flattenContract'
 const DEPLOYED_ADDRESS_ARG = '<deployedAddress>'
 const TOKENS_TO_SUPPORT_ARG = '<tokensToSupport>'
 const SHOW_SUGGESTED_FEES_CMD = 'showSuggestedFees'
+const GET_SUPPORTED_TOKENS_CMD = 'getSupportedTokens'
 const GET_ENCODED_INIT_ARGS_CMD = 'getEncodedInitArgs'
 const SHOW_EXISTING_CONTRACTS_CMD = 'showExistingContracts'
 
@@ -56,6 +60,8 @@ const USAGE_INFO = `
   ${TOOL_NAME} ${SHOW_SUGGESTED_FEES_CMD}
   ${TOOL_NAME} ${SHOW_EXISTING_CONTRACTS_CMD}
   ${TOOL_NAME} ${GET_PNETWORK_CMD} ${DEPLOYED_ADDRESS_ARG}
+  ${TOOL_NAME} ${GET_WETH_ADDRESS} ${DEPLOYED_ADDRESS_ARG}
+  ${TOOL_NAME} ${GET_SUPPORTED_TOKENS_CMD} ${DEPLOYED_ADDRESS_ARG}
   ${TOOL_NAME} ${VERIFY_VAULT_CMD} ${NETWORK_ARG} ${DEPLOYED_ADDRESS_ARG}
   ${TOOL_NAME} ${SET_PNETWORK_CMD} ${DEPLOYED_ADDRESS_ARG} ${ETH_ADDRESS_ARG}
   ${TOOL_NAME} ${GET_ENCODED_INIT_ARGS_CMD} ${WETH_ADDRESS_ARG} ${TOKENS_TO_SUPPORT_ARG}...
@@ -66,7 +72,9 @@ const USAGE_INFO = `
   ${DEPLOY_VAULT_CMD}           ❍ Deploy the ERC20 vault logic contract.
   ${VERIFY_VAULT_CMD}           ❍ Verify a deployed pToken logic contract.
   ${GET_PNETWORK_CMD}           ❍ Show the pNetwork address of the vault at ${DEPLOYED_ADDRESS_ARG}.
+  ${GET_WETH_ADDRESS}        ❍ Show the wETH address set in the vault at ${DEPLOYED_ADDRESS_ARG}.
   ${FLATTEN_CONTRACT_CMD}       ❍ Flatten the contract in case manual verification is required.
+  ${GET_SUPPORTED_TOKENS_CMD}    ❍ Show list of tokens supprted by the vault at ${DEPLOYED_ADDRESS_ARG}.
   ${GET_ENCODED_INIT_ARGS_CMD}    ❍ Calculate the initializer function arguments in ABI encoded format.
   ${SHOW_EXISTING_CONTRACTS_CMD} ❍ Show list of existing logic contract addresses on various blockchains.
 
@@ -97,6 +105,10 @@ const main = _ => {
     return setPNetwork(CLI_ARGS[DEPLOYED_ADDRESS_ARG], CLI_ARGS[ETH_ADDRESS_ARG])
   else if (CLI_ARGS[GET_PNETWORK_CMD])
     return getPNetwork(CLI_ARGS[DEPLOYED_ADDRESS_ARG])
+  else if (CLI_ARGS[GET_SUPPORTED_TOKENS_CMD])
+    return getSupportedTokens(CLI_ARGS[DEPLOYED_ADDRESS_ARG])
+  else if (CLI_ARGS[GET_WETH_ADDRESS])
+    return getWEthAddress(CLI_ARGS[DEPLOYED_ADDRESS_ARG])
 }
 
 main().catch(_err => console.error('✘', _err.message))
