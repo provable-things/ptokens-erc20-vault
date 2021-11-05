bd0165fbb4d6cb1c4ad8a177d12feb0ad2a2587b
Greg Kapka <greg@oraclize.it>
2021-11-05 12:39:16 +0000

    feat(CLI): <- adds that & flatten function

---
+++ cli.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+require('dotenv').config()
+const { docopt } = require('docopt')
+const { version } = require('./package.json')
+const { flattenContract } = require('./lib/flatten-contract')
+
+const HELP_ARG = '--help'
+const VERSION_ARG = '--version'
+const TOOL_NAME = 'vault-cli.js'
+const FLATTEN_CONTRACT_CMD = 'flattenContract'
+
+const USAGE_INFO = `
+❍ pTokens ERC20 Vault CLI ❍
+
+  Copyright Provable Things 2021
+  Questions: greg@oraclize.it
+
+❍ Info:
+
+  A tool to aid with deployments of & interactions with the upgradeable pToken ERC20 vault logic contract.
+
+  NOTE: The tool requires a '.env' file to exist in the root of the repository with the following info:
+
+    PRIVATE_KEY=<private-key-to-sign-transactions-with>
+    ENDPOINT=<rpc-endpoint-for-blochain-to-interact-with>
+
+❍ Usage:
+  ${TOOL_NAME} ${HELP_ARG}
+  ${TOOL_NAME} ${VERSION_ARG}
+  ${TOOL_NAME} ${FLATTEN_CONTRACT_CMD}
+
+❍ Commands:
+  ${FLATTEN_CONTRACT_CMD}       ❍ Flatten the contract in case manual verification is required.
+
+❍ Options:
+  ${HELP_ARG}                ❍ Show this message.
+  ${VERSION_ARG}             ❍ Show tool version.
+`
+const main = _ => {
+  const CLI_ARGS = docopt(USAGE_INFO, { version })
+  if (CLI_ARGS[FLATTEN_CONTRACT_CMD]) {
+    return flattenContract()
+  }
+}
+
+main().catch(_err => console.error('✘', _err.message))
