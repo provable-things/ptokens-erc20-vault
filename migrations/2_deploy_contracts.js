a0c09ffb268ce19aebb6902e8b0d16e352b555c2
Greg Kapka <greg@oraclize.it>
2021-11-08 15:53:54 +0000

    fix(truffle): <- rm deployment stuff for that

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,54 +1,17 @@
-const {
-  WETH_ADDRESS,
-  TOKENS_TO_SUPPORT,
-} = require('../config')
 const { singletons } = require('@openzeppelin/test-helpers')
 const { deployProxy } = require('@openzeppelin/truffle-upgrades')
+
 require('@openzeppelin/test-helpers/configure')({
   environment: 'truffle',
   provider: web3.currentProvider,
 })
 
-const checkAddress = _address =>
-  web3.utils.isAddress(_address)
-
-const checkWethAddress = _ => {
-  console.info('✔ Checking `WETH_ADDRESS` in `config.json`...')
-  if (!checkAddress(WETH_ADDRESS)) {
-    console.info('✘ `WETH_ADDRESS` in `config.json` is NOT a valid ETH address!')
-    process.exit(1)
-  }
-  console.info('✔ `WETH_ADDRESS` in `config.json` is valid!')
-}
-
-const checkTokensToSupport = _ => {
-  console.info('✔ Checking `TOKENS_TO_SUPPORT` in `config.json`...')
-  if (TOKENS_TO_SUPPORT.length === 0) {
-    console.info('✔ No `TOKENS_TO_SUPPORT` in `config.json`, skipping check!')
-    return
-  }
-  const invalidAddresses = TOKENS_TO_SUPPORT
-    .map(checkAddress)
-    .filter((_addressIsValid, _i) => {
-      if (!_addressIsValid) {
-        console.info(`✘'${TOKENS_TO_SUPPORT[_i]} is NOT a valid ETH address!`)
-        return true
-      }
-    })
-  if (invalidAddresses.length > 0) {
-    console.info('✘  `TOKENS_TO_SUPPORT` in `config.json` contains an invalid ETH address!')
-    process.exit(1)
-  }
-  console.info('✔ `TOKENS_TO_SUPPORT` in `config.json` are valid!')
-}
-
 module.exports = async (_deployer, _network, _accounts) => {
   if (_network.includes('develop')) await singletons.ERC1820Registry(_accounts[0])
-  checkWethAddress()
-  checkTokensToSupport()
+  const wEthAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
   await deployProxy(
     artifacts.require('Erc20Vault'),
-    [ WETH_ADDRESS, TOKENS_TO_SUPPORT ],
+    [ wEthAddress, [ wEthAddress ] ],
     { _deployer, initializer: 'initialize' },
   )
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,3 @@
-const {
-  ENDPOINT,
-  GAS_PRICE,
-  PRIVATE_KEY,
-  ETHERSCAN_API_KEY,
-} = require('./config.json')
 const WalletProvider = require('@truffle/hdwallet-provider')
 
 const GAS_LIMIT = 6e6
@@ -18,65 +12,6 @@ module.exports = {
       network_id: '*',
       websockets: false,
     },
-    rinkeby: {
-      network_id: 4,
-      gas: GAS_LIMIT,
-      websockets: true,
-      gasPrice: GAS_PRICE,
-      provider: _ => getProvider(),
-      confirmations: CONFIRMATIONS,
-    },
-    ethMainnet: {
-      gas: 5e6,
-      network_id: 1,
-      gasPrice: GAS_PRICE,
-      provider: _ => getProvider(),
-      confirmations: CONFIRMATIONS,
-    },
-    ropsten: {
-      network_id: 3,
-      gas: GAS_LIMIT,
-      websockets: true,
-      gasPrice: GAS_PRICE,
-      provider: _ => getProvider(),
-      confirmations: CONFIRMATIONS,
-    },
-    bscMainnet: {
-      network_id: 56,
-      gas: GAS_LIMIT,
-      skipDryRun: true,
-      timeoutBlocks: 500,
-      gasPrice: GAS_PRICE,
-      provider: _ => getProvider(),
-      confirmations: CONFIRMATIONS,
-    },
-    bscTestnet: {
-      gas: 8e6,
-      network_id: 97,
-      skipDryRun: true,
-      timeoutBlocks: 500,
-      provider: _ => getProvider(),
-      gasPrice: GAS_PRICE,
-      confirmations: CONFIRMATIONS,
-    },
-    xDai: {
-      gas: GAS_LIMIT,
-      network_id: 100,
-      skipDryRun: true,
-      timeoutBlocks: 10,
-      gasPrice: GAS_PRICE,
-      provider: _ => getProvider(),
-      confirmations: CONFIRMATIONS,
-    },
-    polygonMaticMainnet: {
-      gas: GAS_LIMIT,
-      network_id: 137,
-      timeoutBlocks: 10,
-      networkCheckTimeout: 10000,
-      gasPrice: GAS_PRICE,
-      provider: _ => getProvider(),
-      confirmations: CONFIRMATIONS,
-    },
   },
   compilers: {
     solc: {
@@ -88,14 +23,5 @@ module.exports = {
         }
       }
     }
-  },
-  mocha: {
-    enableTimeouts: false
-  },
-  plugins: [
-    'truffle-plugin-verify'
-  ],
-  api_keys: {
-    etherscan: ETHERSCAN_API_KEY,
-  },
+  }
 }
